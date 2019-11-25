842d9c75eec2a65e6c373ce9d2b3643dfe9d0245
Alex Shvecov <alex2116355@gmail.com>
2019-11-25 22:09:02 +0700

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 let a = 3;
 let b = 17;
 let c = 96;
+let d = `Hello World ${c} 'Привет' `;
+let e = true;
+let f = false;
+let g = null;
+let i = undefined;
+let h = Symbol()
 
-// a = 2;
-// b = 3
+// // // a = 2;
+// // // b = 3
 
+// // console.log(b);
+// // console.log(a);
+// // console.log(a+c);
+// // console.log(b+a);
+
+console.log(d);
+console.log(typeof d)
 console.log(b);
-console.log(a);
-console.log(a+c);
-console.log(b+a);
+console.log(typeof b)
+console.log(e);
+console.log(typeof e)
+console.log(f);
+console.log(typeof f)
+console.log(g);
+console.log(typeof g)
+console.log(i);
+console.log(typeof i)
+
+
+
+
+
